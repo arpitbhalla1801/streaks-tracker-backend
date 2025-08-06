f9a673e67fad2e728bb75c79612cc2e73607605b
Arpit Bhalla <64216681+arpitbhalla1801@users.noreply.github.com>
2025-08-06 13:23:45 +0530

    [removed] CodeChef and GeeksforGeeks streak check integrations from scheduler

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -2,8 +2,6 @@ import cron from 'node-cron';
 import { sendReminderEmail } from '../email/notifier';
 import { checkForLeetCodeStreak } from '../platforms/leetcode';
 import { checkForCodeforcesStreak } from '../platforms/codeforces';
-import { checkForCodeChefStreak } from '../platforms/codechef';
-import { checkForGFGStreak } from '../platforms/gfg';
 import { checkForGitHubStreak } from '../platforms/github';
 
 cron.schedule('0 23 * * *', async () => {
@@ -12,8 +10,6 @@ cron.schedule('0 23 * * *', async () => {
     const platforms = [
         { name: 'LeetCode', username: process.env.LEETCODE_USERNAME, checker: checkForLeetCodeStreak },
         { name: 'Codeforces', username: process.env.CODEFORCES_USERNAME, checker: checkForCodeforcesStreak },
-        { name: 'CodeChef', username: process.env.CODECHEF_USERNAME, checker: checkForCodeChefStreak },
-        { name: 'GeeksforGeeks', username: process.env.GFG_USERNAME, checker: checkForGFGStreak },
         { name: 'GitHub', username: process.env.GITHUB_USERNAME, checker: checkForGitHubStreak },
     ];
 
